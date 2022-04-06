443cfb567d8647f3bbf60b5f5aee211a9ba9be05
Ryan Goodchild <ryang2320@gmail.com>
2022-04-06 15:58:57 -0400

    starting on the front end for GPUcroncher

---
+++ GPU/GPUCronch Frontend/gpucroncher/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+// Vue.prototype.$http = axios
+axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
